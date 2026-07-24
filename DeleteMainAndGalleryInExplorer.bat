c43c67f7501541aa0dce30ff2aef6b1a7eb62c3f
Gorglar <123326814+Gorglar@users.noreply.github.com>
2026-07-24 20:07:07 +0300

    DeleteMainAndGalleryInExplorer.bat

--- DeleteMainAndGalleryInExplorer.bat
+++ DeleteMainAndGalleryInExplorer.bat
@@ -5,6 +5,5 @@ title Delete Main And Gallery In Explorer
 
 REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{f874310e-b6b7-47dc-bc84-b9e6b38f5903} /f
 REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c} /f
-echo Нажмите любую клавишу для продолжения...
 pause >nul
 goto :eof
